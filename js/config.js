aa6a83377ed47507ffd7a6415ef9d285fa346b1f
ragdemtz7-maker <ragdemtz7@gmail.com>
2025-11-17 00:26:58 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_3d9dAMKXZ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1vd7m5lcdd8r9nr5clfga3rctb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
