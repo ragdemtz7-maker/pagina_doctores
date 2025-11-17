6b1becfd8a7f64dea6ef782f7dff01287d4dec6d
ragdemtz7-maker <ragdemtz7@gmail.com>
2025-11-17 02:51:04 -0600

    Update bd.py

--- backend/bd.py
+++ backend/bd.py
@@ -2,7 +2,7 @@ import pymysql
 
 def get_connection():
     return pymysql.connect(
-        host="database-proyecto-final.ce1g806g8367.us-east-1.rds.amazonaws.com",
+        host="database-proyecto-final.c7qoq2es4loe.us-east-2.rds.amazonaws.com",
         user="admin",
         password="#Citasmedicas123",
         database="citas_medicas"
